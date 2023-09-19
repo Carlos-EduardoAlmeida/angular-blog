30948ca69edd05b32770ead53881d2e3c7fb9619
carlos_eduardo <ceduardoeab10@gmail.com>
2023-09-19 15:34:36 -0300

    compoments and styles createds

---
+++ src/app/components/aside-card/aside-card.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-aside-card',
+  templateUrl: './aside-card.component.html',
+  styleUrls: ['./aside-card.component.css']
+})
+export class AsideCardComponent {
+
+}
